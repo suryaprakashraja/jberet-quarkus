aed0cf6e9b6e43dcb9a5b2fff0048efd48582a76
sekaijin <jyterrien@yahoo.fr>
2022-07-31 17:22:45 +0200

    run native and job termination

--- src/main/java/fr/sekaijin/jberet/RunBatchCommand.java
+++ src/main/java/fr/sekaijin/jberet/RunBatchCommand.java
@@ -4,6 +4,7 @@ import static fr.sekaijin.jberet.core.PropertiesBuilder.EMPTY;
 import static fr.sekaijin.jberet.core.PropertiesBuilder.properties;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -13,6 +14,7 @@ import org.jberet.job.model.JobBuilder;
 import org.jberet.job.model.Step;
 import org.jberet.job.model.StepBuilder;
 import org.jberet.repository.ApplicationAndJobName;
+import org.jberet.runtime.JobExecutionImpl;
 import org.jboss.logging.Logger;
 
 import fr.sekaijin.jberet.job.MockBatchlet;
@@ -23,23 +25,24 @@ import fr.sekaijin.jberet.listener.JobListener;
 import fr.sekaijin.jberet.listener.StepListener;
 import io.quarkiverse.jberet.runtime.QuarkusJobOperator;
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @QuarkusMain
 @Command(name = "runBatch", mixinStandardHelpOptions = true, exitCodeOnSuccess = 12)
-public class RunBatchCommand implements Runnable
-{
+public class RunBatchCommand implements Runnable, QuarkusApplication {
 
 	Logger LOG = Logger.getLogger(RunBatchCommand.class);
+	@Inject
+	CommandLine.IFactory factory;
 
 	@CommandLine.Parameters(paramLabel = "<name>", defaultValue = "picocli", description = "Your view name.")
 	String name;
 
-	@CommandLine.Option(names = "-e, --end", defaultValue = "100")
+	@CommandLine.Option(names = { "-e", "--end" }, defaultValue = "100")
 	int end;
-
 
 	@CommandLine.Option(names = "--restart")
 	int restart;
@@ -50,49 +53,41 @@ public class RunBatchCommand implements Runnable
 	@Inject
 	QuarkusJobOperator jobOperator;
 
-	@ConfigProperty(name="quarkus.application.name")
+	@ConfigProperty(name = "quarkus.application.name")
 	String app;
 
-	@ConfigProperty(name="quarkus.application.version")
+	@ConfigProperty(name = "quarkus.application.version")
 	String version;
 
 	long start() {
 
-		final Step firstStep = new StepBuilder("step1")
-				.itemCount(5)
+		final Step firstStep = new StepBuilder("step1").itemCount(5)
 				.reader(MockReader.NAME, properties().add("end", end).build())
-				.processor(MockProcessor.NAME, properties()
-						.add("resource",List.of("V", "Z", "Y", "X", "W"))
+				.processor(MockProcessor.NAME,
+						properties()
+						.add("resource", List.of("V", "Z", "Y", "X", "W"))
 						.add("failed", failed)
-						.add("int", 15)
-						.build())
-				.writer(MockWriter.NAME, EMPTY)
-				.listener(StepListener.NAME, EMPTY)
+						.add("int", 15).build())
+				.writer(MockWriter.NAME, EMPTY).listener(StepListener.NAME, EMPTY)
 
-				.failOn("FAIL").exitStatus("Step 1 fail")
-				.next("Step2")
-				.build();
+				.failOn("FAIL").exitStatus("Step 1 fail").next("Step2").build();
 
-		final Step secondStep = new StepBuilder("Step2")
-				.batchlet(MockBatchlet.NAME, EMPTY)
-				.listener(StepListener.NAME, EMPTY)
-				.build();
+		final Step secondStep = new StepBuilder("Step2").batchlet(MockBatchlet.NAME, EMPTY)
+				.listener(StepListener.NAME, EMPTY).build();
 
-		Job job = new JobBuilder("myJob")
-				.step(firstStep).step(secondStep)
-				.listener(JobListener.NAME, EMPTY)
-				.build();
+		Job job = new JobBuilder("myJob").step(firstStep).step(secondStep).listener(JobListener.NAME, EMPTY).build();
 
 		ApplicationAndJobName name = new ApplicationAndJobName(app, "myJob");
-		jobOperator.getBatchEnvironment().getJobRepository().addJob(name , job);
+		jobOperator.getBatchEnvironment().getJobRepository().addJob(name, job);
 
 		LOG.info(restart);
 
-		if(0==restart){
+		if (0 == restart) {
 			return jobOperator.start(job, EMPTY);
 		} else {
 			return jobOperator.restart(restart, EMPTY);
 		}
+		
 	}
 
 	@Override
@@ -100,6 +95,14 @@ public class RunBatchCommand implements Runnable
 
 		LOG.infof("Hello %s, go go commando! %d", name, end);
 		long executionId = start();
+        final JobExecutionImpl jobExecution = JobExecutionImpl.class.cast(jobOperator.getJobExecution(executionId));
+		try {
+			jobExecution.awaitTermination(0, TimeUnit.SECONDS);  //no timeout
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Quarkus.asyncExit(jobOperator.getJobExecution(executionId).getBatchStatus().ordinal());
 		Quarkus.waitForExit();
 		LOG.info(app);
 		LOG.info(version);
@@ -108,5 +111,10 @@ public class RunBatchCommand implements Runnable
 		LOG.info(jobOperator.getJobExecution(executionId).getEndTime());
 		LOG.info(jobOperator.getJobExecution(executionId).getExitStatus());
 
+	}
+
+	@Override
+	public int run(String... args) throws Exception {
+		return new CommandLine(this, factory).execute(args);
 	}
 }
--- src/main/java/fr/sekaijin/jberet/listener/JobListener.java
+++ src/main/java/fr/sekaijin/jberet/listener/JobListener.java
@@ -8,31 +8,20 @@ import javax.inject.Named;
 
 import org.jboss.logging.Logger;
 
-import io.quarkiverse.jberet.runtime.QuarkusJobOperator;
-import io.quarkus.runtime.Quarkus;
-
 @Named(JobListener.NAME)
 @Dependent
 public class JobListener extends AbstractJobListener {
-
 
 	public static final String NAME = "myJobListener";
 
 	Logger LOGGER = Logger.getLogger(JobListener.class);
 
 	@Inject
-	QuarkusJobOperator jobOperator;
-
-    @Inject
-    JobContext jobContext;
-
+	JobContext jobContext;
 
 	@Override
 	public void afterJob() throws Exception {
 		LOGGER.infof("afterJob %s", jobContext.getJobName());
-
-		int code = jobOperator.getJobExecution(jobContext.getExecutionId()).getBatchStatus().ordinal();
-		Quarkus.asyncExit(code);
 	}
 
 }
